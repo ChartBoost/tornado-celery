1d05303ffa115ca729d603be4e1ed475b8a6a93b
Tucker Joseph <tucker@chartboost.com>
2014-07-30 15:02:46 -0700

    overrode publish task method

--- tcelery/producer.py
+++ tcelery/producer.py
@@ -1,14 +1,21 @@
 from __future__ import absolute_import
 
 import sys
+import numbers
 
 from functools import partial
 from datetime import timedelta
 
 from kombu import serialization
 
+from kombu.utils import uuid
 from celery.app.amqp import TaskProducer
-from celery.backends.amqp import AMQPBackend
+from kombu import Exchange
+from kombu.common import Broadcast
+from celery import signals
+from celery.utils.timeutils import to_utc
+from kombu.utils.encoding import safe_repr
+from celery.five import string_t
 from celery.utils import timeutils
 from tornado.concurrent import Future
 
@@ -97,6 +104,138 @@ class NonBlockingTaskProducer(TaskProducer):
         if value is not None and type:
             return type(value * 1000)
         return value
+
+    def publish_task(self, task_name, task_args=None, task_kwargs=None,
+                     countdown=None, eta=None, task_id=None, group_id=None,
+                     taskset_id=None,  # compat alias to group_id
+                     expires=None, exchange=None, exchange_type=None,
+                     event_dispatcher=None, retry=None, retry_policy=None,
+                     queue=None, now=None, retries=0, chord=None,
+                     callbacks=None, errbacks=None, routing_key=None,
+                     serializer=None, delivery_mode=None, compression=None,
+                     reply_to=None, time_limit=None, soft_time_limit=None,
+                     declare=None, headers=None,
+                     send_before_publish=signals.before_task_publish.send,
+                     before_receivers=signals.before_task_publish.receivers,
+                     send_after_publish=signals.after_task_publish.send,
+                     after_receivers=signals.after_task_publish.receivers,
+                     send_task_sent=signals.task_sent.send,  # XXX deprecated
+                     sent_receivers=signals.task_sent.receivers,
+                     **kwargs):
+        """Send task message."""
+        retry = self.retry if retry is None else retry
+        headers = {} if headers is None else headers
+
+        qname = queue
+        if queue is None and exchange is None:
+            queue = self.default_queue
+        if queue is not None:
+            if isinstance(queue, string_t):
+                qname, queue = queue, self.queues[queue]
+            else:
+                qname = queue.name
+            exchange = exchange or queue.exchange.name
+            routing_key = routing_key or queue.routing_key
+        if declare is None and queue and not isinstance(queue, Broadcast):
+            declare = [queue]
+        if delivery_mode is None:
+            delivery_mode = self._default_mode
+
+        # merge default and custom policy
+        retry = self.retry if retry is None else retry
+        _rp = (dict(self.retry_policy, **retry_policy) if retry_policy
+               else self.retry_policy)
+        task_id = task_id or uuid()
+        task_args = task_args or []
+        task_kwargs = task_kwargs or {}
+        if not isinstance(task_args, (list, tuple)):
+            raise ValueError('task args must be a list or tuple')
+        if not isinstance(task_kwargs, dict):
+            raise ValueError('task kwargs must be a dictionary')
+        if countdown:  # Convert countdown to ETA.
+            now = now or self.app.now()
+            eta = now + timedelta(seconds=countdown)
+            if self.utc:
+                eta = to_utc(eta).astimezone(self.app.timezone)
+        if isinstance(expires, numbers.Real):
+            now = now or self.app.now()
+            expires = now + timedelta(seconds=expires)
+            if self.utc:
+                expires = to_utc(expires).astimezone(self.app.timezone)
+        eta = eta and eta.isoformat()
+        expires = expires and expires.isoformat()
+
+        body = {
+            'task': task_name,
+            'id': task_id,
+            'args': task_args,
+            'kwargs': task_kwargs,
+            'retries': retries or 0,
+            'eta': eta,
+            'expires': expires,
+            'utc': self.utc,
+            'callbacks': callbacks,
+            'errbacks': errbacks,
+            'timelimit': (time_limit, soft_time_limit),
+            'taskset': group_id or taskset_id,
+            'chord': chord,
+        }
+
+        if before_receivers:
+            send_before_publish(
+                sender=task_name, body=body,
+                exchange=exchange,
+                routing_key=routing_key,
+                declare=declare,
+                headers=headers,
+                properties=kwargs,
+                retry_policy=retry_policy,
+            )
+
+        publish_future = self.publish(
+            body,
+            exchange=exchange, routing_key=routing_key,
+            serializer=serializer or self.serializer,
+            compression=compression or self.compression,
+            headers=headers,
+            retry=retry, retry_policy=_rp,
+            reply_to=reply_to,
+            correlation_id=task_id,
+            delivery_mode=delivery_mode, declare=declare,
+            **kwargs
+        )
+
+        if after_receivers:
+            send_after_publish(sender=task_name, body=body,
+                               exchange=exchange, routing_key=routing_key)
+
+        if sent_receivers:  # XXX deprecated
+            send_task_sent(sender=task_name, task_id=task_id,
+                           task=task_name, args=task_args,
+                           kwargs=task_kwargs, eta=eta,
+                           taskset=group_id or taskset_id)
+        if self.send_sent_event:
+            evd = event_dispatcher or self.event_dispatcher
+            exname = exchange or self.exchange
+            if isinstance(exname, Exchange):
+                exname = exname.name
+            evd.publish(
+                'task-sent',
+                {
+                    'uuid': task_id,
+                    'name': task_name,
+                    'args': safe_repr(task_args),
+                    'kwargs': safe_repr(task_kwargs),
+                    'retries': retries,
+                    'eta': eta,
+                    'expires': expires,
+                    'queue': qname,
+                    'exchange': exname,
+                    'routing_key': routing_key,
+                },
+                self, retry=retry, retry_policy=retry_policy,
+            )
+        return publish_future
 
     def __repr__(self):
         return '<NonBlockingTaskProducer: {0.channel}>'.format(self)
