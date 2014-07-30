d9dcc3f1ad7f8a1cc3368784b5161d70f9f9be96
Gregory Rice <gregrice@gmail.com>
2014-07-30 14:03:11 -0700

    Add async producer

--- tcelery/app.py
+++ tcelery/app.py
@@ -6,10 +6,44 @@ from tornado import web
 from tornado.options import define, options
 
 from . import handlers as _ # noqa
+from celery import uuid
+from celery._state import get_current_worker_task
+from celery.utils.functional import maybe_evaluate, maybe_list
 from .utils import route
+from tornado.concurrent import Future
 
 
 define("debug", type=bool, default=False, help="run in debug mode")
+
+class AsyncCelery(celery.Celery):
+
+    def send_task(self, name, args=None, kwargs=None, countdown=None,
+                  eta=None, task_id=None, producer=None, connection=None,
+                  router=None, result_cls=None, expires=None,
+                  publisher=None, link=None, link_error=None,
+                  add_to_parent=True, reply_to=None, **options):
+        task_id = task_id or uuid()
+        producer = producer or publisher  # XXX compat
+        router = router or self.amqp.router
+        conf = self.conf
+
+        options = router.route(options, name, args, kwargs)
+        if connection:
+            producer = self.amqp.TaskProducer(connection)
+        with self.producer_or_acquire(producer) as P:
+            self.backend.on_task_call(P, task_id)
+            task_id = P.publish_task(
+                name, args, kwargs, countdown=countdown, eta=eta,
+                task_id=task_id, expires=expires,
+                callbacks=maybe_list(link), errbacks=maybe_list(link_error),
+                reply_to=reply_to or self.oid, **options
+            )
+        result = (result_cls or self.AsyncResult)(task_id)
+        if add_to_parent:
+            parent = get_current_worker_task()
+            if parent:
+                parent.add_trail(result)
+        return result
 
 
 class Application(web.Application):
